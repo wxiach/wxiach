13daad77cc5a95b5c89ff84c60a9903db5f9446c
wxiach <wxiach@gmail.com>
2025-02-20 00:40:16 +0800

    chore: adjust content collections's name

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const post = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/post" }),
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
   schema: z.object({ title: z.string(), date: z.coerce.date(), category: z.string() }),
 });
 
-const musing = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/musing" }),
+const musings = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/musings" }),
   schema: z.object({ date: z.coerce.date() }),
 });
 
-export const collections = { post, musing };
+export const collections = { posts, musings };
