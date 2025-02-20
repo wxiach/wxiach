1152af48e71e878fd283814e65522800fdf20780
wxiach <wxiach@gmail.com>
2025-02-20 20:39:54 +0800

    chore: add some musings and adjust site config

--- src/consts.ts
+++ src/consts.ts
@@ -1,15 +1,17 @@
 export const site = {
-  website: "blog.weixiaochen.cn",
-  title: "wxiach",
-  description: "一个迷茫的小孩。",
+  website: "wxiach.cn",
+  title: "wxiach's blog",
+  description: "",
   author: "wxiach",
+  hitokoto: "一个迷茫的小孩。",
+  introduction:
+    "鄙人生性愚钝，闲静少言，不慕荣利，好Coding，不求甚解，每有会意，便懒于外卖。性宅舍，以课不能常得，故弃课而憩之，同舍知其如此，或劝之。常著Bug自娱，颇示己志。",
 };
 
-export const email = "weixiaochen0914@gmail.com";
+export const email = "wxiach@gmail.com";
 
 export const social = {
   github: "https://github.com/wxiach",
-  twitter: "https://twitter.com/aozTLnIBES1renM",
 };
 
 export const nav = [
