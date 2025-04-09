d0f6948ff61d665f3fa6c3f3768eb2a335fc1fdf
wxiach <wxiach@gmail.com>
2025-04-09 23:19:40 +0800

    feat: add fade-in animation

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {},
+  },
+  // You might need content paths if not using defaults
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+}
