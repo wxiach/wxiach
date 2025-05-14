c4a167fda3f08f8bc0f95c84e7442a6dcfbca528
wxiach <wxiach@gmail.com>
2025-05-14 23:03:19 +0800

    fix(ui): delete error gap bettwen fotter and broswer window bottom

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,19 @@
 export const site = {
-  title: "wxiach's blog",
+  title: "无畏小生的茶馆",
   description:
     "wxiach's personal website - 一个享受独处与简约生活的程序员，记录思考、成长与代码。探索技术、随笔、生活感悟，偶尔回顾过往。欢迎交流，共同进步。",
-  author: "wxiach",
+  author: "Soren  Wei",
   hitokoto: [
     "一个迷茫的小孩。",
     "孤独症患者。",
     "享受独处的人。",
     "热爱生活的代码工匠。",
-    "思考与反思中前行。"
+    "思考与反思中前行。",
   ],
   contact: [{ email: "wxiach@gmail.com" }],
   nav: [
     { name: "GitHub", url: "https://github.com/wxiach" },
+    { name: "Notes", url: "https://notes.wxiach.cn" },
     { name: "Twitter", url: "https://twitter.com/iwxiach" },
-    { name: "Weibo", url: "https://weibo.com/u/7889488509" },
   ],
 };
