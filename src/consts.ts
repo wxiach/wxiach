711b0470f3a6b30a5fe44b848e21444718571507
wxiach <wxiach@gmail.com>
2025-08-03 18:48:22 +0800

    style: modify header style

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,17 @@
 export const site = {
-  title: "Soren's Blog",
+  title: "无畏小生的博客",
   description:
-    "Soren Wei的个人博客，专注于分享编程技术、代码心得、个人成长感悟与生活随笔。探索简约生活，记录程序员的思考与点滴。",
-  author: "Soren  Wei",
-  hitokoto: ["一个迷茫的小孩。", "孤独症患者。", "享受独处的人。", "热爱生活的代码工匠。", "思考与反思中前行。"],
+    "无畏小生的个人博客，专注于分享编程技术、代码心得、个人成长感悟与生活随笔。探索简约生活，记录程序员的思考与点滴。",
+  author: "无畏小生",
+  hitokoto: ["一个迷茫的小孩", "孤独症患者", "享受独处的人", "热爱生活的代码工匠", "思考与反思中前行"],
   contact: [{ email: "wxiach@gmail.com" }],
   nav: [
+    { name: "首页", url: "/" },
+    { name: "文章", url: "/posts" },
+    { name: "关于我", url: "/about" },
+  ],
+  social: [
     { name: "GitHub", url: "https://github.com/wxiach" },
-    { name: "Notes", url: "https://notes.wxiach.cn" },
     { name: "Twitter", url: "https://twitter.com/iwxiach" },
   ],
 };
