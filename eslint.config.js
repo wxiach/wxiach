bd6fa482a331a2e7172436e034c8f9d00bd3c502
wxiach <wxiach@gmail.com>
2025-08-10 23:40:01 +0800

    chore: add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,29 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+
 export default [
+  // 继承 astro 推荐配置
   ...eslintPluginAstro.configs.recommended,
+
+  // 自定义配置
   {
-    rules: {},
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "./tsconfig.json", // 你的tsconfig路径
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    extends: ["plugin:@typescript-eslint/recommended"],
+    rules: {
+      // 你自己的规则
+    },
+    overrides: [
+      {
+        files: ["*.astro"],
+        parser: "astro-eslint-parser",
+        parserOptions: {
+          parser: "@typescript-eslint/parser",
+        },
+      },
+    ],
   },
 ];
