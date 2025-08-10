b14085a289d64f1d12327859e12e5433a8ccc797
wxiach <wxiach@gmail.com>
2025-08-10 22:44:52 +0800

    chore: code format

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
