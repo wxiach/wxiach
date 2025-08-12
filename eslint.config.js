185d7725dd38509ed512965d125decc997c054e6
wxiach <wxiach@gmail.com>
2025-08-12 15:05:36 +0800

    chore: format code and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
 
-// Official recommended configuration.
 export default tseslint.config(...eslintPluginAstro.configs.recommended);
